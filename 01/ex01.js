ccbd451fb0f5f901858e3a70de5b1c16891ed334
Daniel bruno <danielbrunodsn@gmail.com>
2023-08-09 15:31:43 -0300

    resolvido

---
+++ 01/ex01.js
@@ -0,0 +1,19 @@
+const endereco = {
+    rua: 'Dragon Ball Z',
+    numero: 001,
+    complemento: 'Próxima a Escola Naruto',
+    bairro: 'Tv globiho',
+    cep: 00022-555,
+    cidade: 'Tokyo',
+    pais: 'Japão'
+}
+
+const usuario = {
+    nome: 'Son Goku',
+    email: 'goku.dbz@otmail.com',
+    telefone: 559665544,
+    data_nascimento: '02-05-1992'
+}
+
+usuario.endereco = endereco
+console.log(usuario)
